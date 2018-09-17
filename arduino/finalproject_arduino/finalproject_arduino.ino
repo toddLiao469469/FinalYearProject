06cede643b82d789292e502cfaa87935530ebf73
廖志融 <liaozhirong@liaozhirongde-MacBook-Pro.local>
2018-09-17 16:10:24 +0800

    pulse push to android app sucess

--- arduino/finalproject_arduino/finalproject_arduino.ino
+++ arduino/finalproject_arduino/finalproject_arduino.ino
@@ -4,13 +4,13 @@
 
 #define blueTooth_RX 8
 #define blueTooth_TX 7
-#define PulseWire A0
+#define PulseWire A1
 #define LED13 13
-#define BPMCount 100
+#define BPMCount 20
 
 int Threshold = 550;
 SoftwareSerial BT(blueTooth_RX,blueTooth_TX);   // 接收腳(RX), 傳送腳(TX)；接HC-06之TXD、RXD   
-  
+PulseSensorPlayground pulseSensor;
 char val;  
 String recieveData = "";   
 bool startRecieve = false;  
@@ -21,11 +21,7 @@ void setup()
 {  
   Serial.begin(9600);   
   BT.begin(9600);       //HC-06 預設 baud  
-<<<<<<< HEAD
   Serial.println("branchtest");
-=======
-  Serial.println("branch");
->>>>>>> arduino
 
   pulseSensor.analogInput(PulseWire);   
   pulseSensor.blinkOnPulse(LED13);       
@@ -35,13 +31,17 @@ void setup()
    if (pulseSensor.begin()) {
     Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.  
   }
+  
+  Serial.print("21312321");
+  BT.write("A");
 }  
   
 void loop()  
 {  
   int myBPM = pulseSensor.getBeatsPerMinute();  
-  myBPMAverage += myBPM;
-
+  if(myBPM <=255 && myBPM >=0){
+    myBPMAverage += myBPM;
+  }
   if (pulseSensor.sawStartOfBeat()) {             
     Serial.print("BPM: ");                        
     Serial.println(myBPM);                         
@@ -49,22 +49,33 @@ void loop()
 
   if(myBPMCount < BPMCount){
     myBPMCount++;
-  }else if(myBPMCount >= BPMCount){
-    myBPMAverage /= 100;
   }
-  delay(20);                    
+  if(myBPMCount >= BPMCount){
+    myBPMAverage /= myBPMCount;
+  }
+  Serial.print("test aver:");
+   Serial.println(myBPMAverage);
 
-  while(myBPMCount >= BPMCount){ //如果有收到資料  
+  delay(800);                    
+
+  if(myBPMCount >= BPMCount){ //如果有收到資料  
     char pushBPM[3];
-    pushBPM[0] =char((myBPMAverage % 1000)- (myBPMAverage % 100) /100)
-    pushBPM[1] =char((myBPMAverage % 100)- (myBPMAverage % 10) /10)
-    pushBPM[2] =char(myBPMAverage % 10)
-    
+    pushBPM[0] =((myBPMAverage % 1000)- (myBPMAverage % 100))/100 + '0';
+    pushBPM[1] =((myBPMAverage % 100)- (myBPMAverage % 10))/10 + '0';
+    pushBPM[2] =(myBPMAverage % 10) + '0';
+     Serial.println("");
+     Serial.print("aver:");
+     Serial.println(myBPMAverage);
+     myBPMCount=0;
+     myBPMAverage=0;
     for(int i = 0 ; i < 3 ; i++){
+      Serial.println(pushBPM[i]);
+      delay(500);
       BT.write(byte(pushBPM[i])); 
-      delay(200);
     }
   
   }  
   
 }  
+
+
